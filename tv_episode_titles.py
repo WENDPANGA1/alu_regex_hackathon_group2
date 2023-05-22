357a86c3e859a3ba5ccf81ec7d6407fc1f7f6600
cynthianekesa <c.nekesa@alustudent.com>
2023-05-22 12:37:05 -0700

    tv_episode_titles

---
+++ tv_episode_titles.py
@@ -0,0 +1,19 @@
+import re
+
+pattern = r'^(.+)\sS(\d{2})E(\d{2}): (.+)$'
+string = "Show Name S01E02: Episode Title"
+
+match = re.match(pattern, string)
+if match:
+    show_name = match.group(1)
+    season_number = match.group(2)
+    episode_number = match.group(3)
+    episode_title = match.group(4)
+    
+    print("Show Name:", show_name)
+    print("Season Number:", season_number)
+    print("Episode Number:", episode_number)
+    print("Episode Title:", episode_title)
+else:
+    print("No match found.")
+
---
+++ tv_episode_titles.rb
@@ -0,0 +1,2 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/^(.+)\sS(\d{2})E(\d{2}): (.+)$/).join
