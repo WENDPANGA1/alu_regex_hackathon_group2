ca375886a7d66173923dd263abd844c3582edac8
WENDPANGA1 <w.ima@alustudent.com>
2023-05-23 04:46:58 -0700

    song lyrics

--- lyrics.py
+++ lyrics.py
@@ -2,9 +2,9 @@
 
 import re
 # song lyrics string
-input_string = "[Verse 1] Lift me up hold me down\s [Verse 2] ke
+input_string = '[Verse 1] Lift me up hold me down\s [Verse 2] ke
 ep me close safe and sound\s [Verse 3] Burning in a hopele
-ss dream hold me when you go to sleep"
+ss dream hold me when you go to sleep'
 
 #Regular expression pattern
 pattern = '\[(Verse \d+)\] (.*?)'
@@ -15,4 +15,4 @@ matches = re.findall(pattern, input_string)
 for match in matches:
     verse_label = match[0]
     lyrics = match[1]
-    print(f"{verse_label}: {lyrics}")
+    print(f'{verse_label}: {lyrics}')
